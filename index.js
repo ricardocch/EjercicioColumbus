1deb58b83f9cb383f49248b2ee8fe93cb8665956
Ricardo HP <ricardo_azul4@yahoo.com>
2022-05-21 17:37:58 -0500

    Se crea funcion para obtener arreglo lineal

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-function getArray(arr) {
-  //  codigo 
-  return [1, 2, 3, 4, 5];
+function getArray(inputArray) {
+
+  let outputArray = [];
+  inputArray.forEach(element => {
+    if(Array.isArray(element)){
+        let newArray = getArray(element)
+        outputArray = outputArray.concat(newArray)
+    } else {
+      outputArray.push(element)
+    }
+
+  });
+  return outputArray;
 }
 
-
+let exampleArray = [6, [1, [2, 3], 4], 5];
+console.log(getArray(exampleArray))
 
 
 module.exports = {
